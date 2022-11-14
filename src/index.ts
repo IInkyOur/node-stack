3b481895a7f0caa19f5f477b03d2eada20da3a12
Yukinori Eguchi <yukinori.eguchi@gmail.com>
2022-11-14 18:35:23 +0900

    Enable debugger in Docker

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ const app = express();
 app.use(morgan('dev')); // logger
 
 app.get('/', async (req, res) => {
-  res.json({ hello: 'yukino' });
+  res.json({ hello: 'world' });
 });
 
 const port = Number(process.env.PORT ?? 8080);
