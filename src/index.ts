372846dabec4d9b51623076a597fef23a0e4ef4f
Yukinori Eguchi <yukinori.eguchi@gmail.com>
2022-11-17 15:10:28 +0900

    Query database, seed data

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,38 @@
+import { PrismaClient } from '@prisma/client';
 import express from 'express';
 import morgan from 'morgan';
+import { nanoid } from 'nanoid';
+
+const db = new PrismaClient({ log: ['error', 'info', 'query', 'warn'] });
+const genId = () => nanoid(16);
+
+const seedDatabase = async () => {
+  if ((await db.post.count()) === 0) {
+    await db.post.createMany({
+      data: [
+        {
+          id: genId(),
+          slug: 'node-stack',
+          title: 'Node Stack 2022',
+          publishedAt: new Date(),
+        },
+        {
+          id: genId(),
+          slug: 'post-test',
+          title: 'Post test',
+        },
+      ],
+    });
+  }
+};
+seedDatabase();
 
 const app = express();
 app.use(morgan('dev')); // logger
 
 app.get('/', async (req, res) => {
-  res.json({ hello: 'world' });
+  const posts = await db.post.findMany();
+  res.json(posts);
 });
 
 const port = Number(process.env.PORT ?? 8080);
